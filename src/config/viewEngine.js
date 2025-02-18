ca3151b39417b1920834bb8ac4115619458807b5
ducpham <phamcongducx9@gmail.com>
2025-02-18 17:55:39 +0700

    config static files

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,7 +1,12 @@
+const express = require('express')
 
 const configViewEngine = (app) => {
+    app.use(express.static('src/public'))
+
     app.set('view engine', 'ejs')
     app.set('views', 'src/views')
 }
+
+
 
 module.exports = configViewEngine
